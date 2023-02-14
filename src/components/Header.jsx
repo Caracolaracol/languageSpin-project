aab1a02b76747db2b1474784e0520415bcb662b9
Agustin Rojas <87249022+Caracolaracol@users.noreply.github.com>
2023-02-14 10:57:26 -0300

    header (#7)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 function Header() {
   return (
-    <div className='flex '>
+    <div className='flex flex-row flex-grow justify-between items-center mx-4 my-2 border-b-2 md:mx-10 md:my-6 pb-3 md:pb-4  '>
       <div>
-        <img className='w-32 h-32' src={logo} />
+        <Link to='/' >
+          <img className='w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logo} />
+        </Link>
+        
       </div>
-      <h1>Language Spin Idiomas</h1>
+      <div className='flex'>
+        <h1 className='self-center text-lg md:text-2xl'>Language Spin Idiomas</h1>
+      </div>
     </div>
   );
 }
