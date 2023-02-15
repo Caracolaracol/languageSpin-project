b1500f9779a89ce1108f6c8bed9c6c327e0db003
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-02-15 16:00:00 -0300

    header fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,16 @@ import logo from '../assets/logo.png';
 
 function Header() {
   return (
-    <div className='flex flex-row flex-grow justify-between items-center mx-4 my-2 border-b-2 md:mx-10 md:my-6 pb-3 md:pb-4  '>
+    <div className='flex justify-between flex-row items-center mx-4 my-2 border-b-2 h-[9rem] md:h-[12rem] xl:h-[14.5rem] md:mx-10 md:my-6 pb-3 md:pb-4  '>
       <div>
         <Link to='/'>
           <img className='w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logo} />
         </Link>
       </div>
-      <div className='flex'>
-        <h1 className='self-center font-semibold text-lg md:text-2xl'>Language Spin Idiomas</h1>
+      <div className='h-[4rem] md:h-[8rem] flex flex-col justify-center'>
+          <h1 className='text-lg font-bold align-text-bottom md:text-4xl md:leading-[4rem]' >Language Spin Idiomas</h1>
+
+          
       </div>
     </div>
   );
