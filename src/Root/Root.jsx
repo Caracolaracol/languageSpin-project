7d1a044cb17c13fe0d79ae25dae62dd8f6e2ae32
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-02-18 22:20:51 -0300

    navbar fix mediaquery

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -2,13 +2,17 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
+import { Container } from '../components/utils/utils';
 
 const Root = () => {
   return (
     <div className='bg-zinc-100 min-h-screen flex flex-col justify-between'>
       <div>
         <Header/>
-        <NavBar />
+          <Container>
+            <NavBar />
+          </Container>
+        
         <Outlet />
       </div>
       <Footer/>
