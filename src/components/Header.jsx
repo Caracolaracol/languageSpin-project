2b981bb5816c74e4c3f607c7ee8349566deec4b2
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-02-28 17:27:34 -0300

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
-
+import logosintexto1 from '../assets/logosintexto1.png'
+import logosintexto2 from '../assets/logosintexto2.png'
 function Header() {
   return (
     <div className='flex justify-between flex-row items-center mx-4 my-2 border-b-2 h-[9rem] md:h-[12rem] xl:h-[14.5rem] md:mx-10 md:my-6 pb-3 md:pb-4  '>
       <div>
         <Link to='/'>
-          <img className='w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logo} />
+          <img className='hidden sm:block w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logo} />
+          <img className='sm:hidden w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logosintexto1} />
         </Link>
       </div>
       <div className='h-[4rem] md:h-[8rem] flex flex-col justify-center'>
