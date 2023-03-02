7c562f75b7ec2d7e6ab9b8772cf8c046d7ce8d92
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-03-02 19:12:39 -0300

    nosotros, slider

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import { Container } from '../components/utils/utils';
-
+import "../index.css";
 const Root = () => {
   return (
     <div className='bg-zinc-100 min-h-screen flex flex-col justify-between'>
