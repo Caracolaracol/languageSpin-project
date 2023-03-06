98522409e9a381cd88b656d5f99db6337f632549
Pablo Valente <pablo.nvalente@gmail.com>
2023-03-06 16:09:09 -0300

    saving stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
-import logosintexto1 from '../assets/logosintexto1.png'
-import logosintexto2 from '../assets/logosintexto2.png'
+import logosintexto1 from '../assets/logosintexto1.png';
+
 function Header() {
   return (
-    <div className='flex justify-between flex-row items-center mx-4 my-2 border-b-2 h-[9rem] md:h-[12rem] xl:h-[14.5rem] md:mx-10 md:my-6 pb-3 md:pb-4  '>
+    <div className='flex justify-between items-center pl-2 pr-4 pt-1 border-b border-zinc-300 shadow-sm'>
       <div>
         <Link to='/'>
           <img className='hidden sm:block w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logo} />
-          <img className='sm:hidden w-32 h-32 md:w-40 md:h-40 xl:w-52 xl:h-52 ' src={logosintexto1} />
+          <img
+            className='sm:hidden w-[90px] h-[90px] md:w-40 md:h-40 xl:w-52 xl:h-52'
+            src={logosintexto1}
+          />
         </Link>
       </div>
-      <div className='h-[4rem] md:h-[8rem] flex flex-col justify-center'>
-          <h1 className='text-lg font-bold align-text-bottom md:text-5xl md:leading-[4rem] opacity-90 drop-shadow-xl '>Language Spin Idiomas</h1>
-
-          
+      <div className='flex flex-col justify-center'>
+        <p className='text-2xl font-extrabold md:text-5xl md:leading-[4rem]'>Language Spin</p>
+        <p className='text-[10px] md:text-base text-zinc-600 font-bold uppercase text-right'>
+          Academia de Idiomas
+        </p>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,30 +2,45 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div className='my-8 flex justify-center'>
-      <ul className='flex items-center gap-3 sm:gap-6'>
-        <li>
-          <NavLink to='/ingles' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
-            Inglés
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/italiano' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
-            Italiano
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='/portugues'
-            className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
-            Portugués
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/espanol' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
-            Español
-          </NavLink>
-        </li>
+    <div className='my-4'>
+      <ul className='flex items-center justify-between'>
+        <NavLink to='/ingles' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
+          <li className='flex flex-col items-center justify-center'>
+            <img
+              src='./src/assets/uk-flag.png'
+              className='border border-zinc-300 w-16 h-16 object-cover rounded-full'
+            />
+            <p className='text-sm'>Inglés</p>
+          </li>
+        </NavLink>
+        <NavLink to='/italiano' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
+          <li className='flex flex-col items-center justify-center'>
+            <img
+              src='./src/assets/it-flag.png'
+              className='border border-zinc-300 w-16 h-16 object-cover rounded-full'
+            />
+            <p className='text-sm'>Italiano</p>
+          </li>
+        </NavLink>
+        <NavLink to='/portugues' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
+          <li className='flex flex-col items-center justify-center'>
+            <img
+              src='./src/assets/pt-flag.png'
+              className='border border-zinc-300 w-16 h-16 object-cover rounded-full'
+            />
+
+            <p className='text-sm'>Portugués</p>
+          </li>
+        </NavLink>
+        <NavLink to='/espanol' className={({ isActive }) => (isActive ? 'active' : 'notactive')}>
+          <li className='flex flex-col items-center justify-center'>
+            <img
+              src='./src/assets/es-flag.png'
+              className='border border-zinc-300 w-16 h-16 object-cover rounded-full'
+            />
+            <p className='text-sm'>Español</p>
+          </li>
+        </NavLink>
       </ul>
     </div>
   );
