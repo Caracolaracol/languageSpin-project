b4347cf94e85a47c78e3dbf0fa4b6d671d3cbdfc
Pablo Valente <pablo.nvalente@gmail.com>
2023-03-06 18:00:02 -0300

    final commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,92 +3,95 @@ import ukFlag from '../assets/united-kingdom.png';
 import itFlag from '../assets/italy.png';
 import brFlag from '../assets/brazil.png';
 import arFlag from '../assets/argentina.png';
+import { Container } from '../components/utils/utils';
 
 function NavBar() {
   return (
-    <div className='my-4 md:my-8'>
-      <ul className='flex items-center justify-between md:justify-center md:gap-20'>
-        <li>
-          <NavLink to='/ingles' className='flex flex-col items-center justify-center'>
-            {({ isActive }) => (
-              <>
-                <img
-                  src={ukFlag}
-                  className={`${
-                    isActive ? '' : 'brightness-[0.85]'
-                  }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-100`}
-                />
-                <p
-                  className={`text-sm md:text-base mt-1 font-bold text-zinc-600 ${
-                    isActive && 'text-verdeSpin'
-                  }`}>
-                  Inglés
-                </p>
-              </>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/italiano' className='flex flex-col items-center justify-center'>
-            {({ isActive }) => (
-              <>
-                <img
-                  src={itFlag}
-                  className={`${
-                    isActive ? '' : 'brightness-[0.85]'
-                  }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-100`}
-                />
-                <p
-                  className={`text-sm md:text-base mt-1 font-bold text-zinc-600 ${
-                    isActive && 'text-verdeSpin'
-                  }`}>
-                  Italiano
-                </p>
-              </>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/portugues' className='flex flex-col items-center justify-center'>
-            {({ isActive }) => (
-              <>
-                <img
-                  src={brFlag}
-                  className={`${
-                    isActive ? '' : 'brightness-[0.85]'
-                  }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-100`}
-                />
-                <p
-                  className={`text-sm md:text-base mt-1 font-bold text-zinc-600 ${
-                    isActive && 'text-verdeSpin'
-                  }`}>
-                  Portugués
-                </p>
-              </>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/espanol' className='flex flex-col items-center justify-center'>
-            {({ isActive }) => (
-              <>
-                <img
-                  src={arFlag}
-                  className={`${
-                    isActive ? '' : 'brightness-[0.85]'
-                  }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-100`}
-                />
-                <p
-                  className={`text-sm md:text-base mt-1 font-bold text-zinc-600 ${
-                    isActive && 'text-verdeSpin'
-                  }`}>
-                  Español
-                </p>
-              </>
-            )}
-          </NavLink>
-        </li>
-      </ul>
+    <div className='bg-verdeSpin py-4 md:py-8 md:mb-12'>
+      <Container>
+        <ul className='flex items-center justify-between md:justify-center md:gap-20'>
+          <li>
+            <NavLink to='/ingles' className='flex flex-col items-center justify-center'>
+              {({ isActive }) => (
+                <>
+                  <img
+                    src={ukFlag}
+                    className={`${
+                      isActive && 'brightness-125'
+                    }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-110`}
+                  />
+                  <p
+                    className={`text-sm md:text-base mt-2 px-2 rounded font-bold text-white ${
+                      isActive ? 'bg-rojoSpin' : 'bg-transparent'
+                    }`}>
+                    Inglés
+                  </p>
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/italiano' className='flex flex-col items-center justify-center'>
+              {({ isActive }) => (
+                <>
+                  <img
+                    src={itFlag}
+                    className={`${
+                      isActive && 'brightness-125'
+                    }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-110`}
+                  />
+                  <p
+                    className={`text-sm md:text-base mt-2 px-2 rounded font-bold text-white ${
+                      isActive ? 'bg-rojoSpin' : 'bg-transparent'
+                    }`}>
+                    Italiano
+                  </p>
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/portugues' className='flex flex-col items-center justify-center'>
+              {({ isActive }) => (
+                <>
+                  <img
+                    src={brFlag}
+                    className={`${
+                      isActive && 'brightness-125'
+                    }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-110`}
+                  />
+                  <p
+                    className={`text-sm md:text-base mt-2 px-2 rounded font-bold text-white ${
+                      isActive ? 'bg-rojoSpin' : 'bg-transparent'
+                    }`}>
+                    Portugués
+                  </p>
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/espanol' className='flex flex-col items-center justify-center'>
+              {({ isActive }) => (
+                <>
+                  <img
+                    src={arFlag}
+                    className={`${
+                      isActive && 'brightness-125'
+                    }  w-16 h-16 md:w-32 md:h-32 object-cover rounded-full transition hover:brightness-110`}
+                  />
+                  <p
+                    className={`text-sm md:text-base mt-2 px-2 rounded font-bold text-white ${
+                      isActive ? 'bg-rojoSpin' : 'bg-transparent'
+                    }`}>
+                    Español
+                  </p>
+                </>
+              )}
+            </NavLink>
+          </li>
+        </ul>
+      </Container>
     </div>
   );
 }
