cd6222a9488d87b0544ed2001fd0502435f3e560
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-03-10 18:44:35 -0300

    fix slider, spain flag

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -23,6 +23,8 @@ import "../index.css";
  
 function Slider() {
 
+    const caruselmobile = [carruselmobile1, carruselmobile2, carruselmobile3]
+
     return (
         <div className='w-[100%] h-[100%] pb-8 pt-8 sm:pt-0'>
             <div className='hidden sm:block'>
@@ -63,70 +65,46 @@ function Slider() {
                             <div className='absolute w-full h-full flex top-0 justify-end items-end p-12 px-16' >
                                 <img className='h-16 ' src={whatsapp} />
                             </div>
-                            
+
                         </div>
                     </SwiperSlide>
                 </Swiper>
+            </div>
 
-
+            <div className='sm:hidden'>
+                <Swiper
+                    style={{
+                        '--swiper-navigation-color': '#ff5756',
+                        '--swiper-pagination-color': '#ff5756',
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    navigation={true}
+                    modules={[Pagination, Navigation, Autoplay]}
+                    className='mySwiper'>
+                        {
+                            caruselmobile.map(img => 
+                                (<SwiperSlide key={img}>
+                                    <div>
+                                        <img className='opt' src={img} />
+                                    </div>
+                                </SwiperSlide>)
+                            
+                            )
+                            
+                        }
+                    
+                </Swiper>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className='flex'>
-              <img src={carrusel2} />
-              <div className='absolute w-full h-full flex top-0 justify-end items-end p-12 px-16'>
-                <img className='h-16 ' src={whatsapp} />
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className='flex'>
-              <img src={carrusel3} />
-              <div className='absolute w-full h-full flex top-0 justify-end items-end p-12 px-16'>
-                <img className='h-16 ' src={whatsapp} />
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-
-      <div className='sm:hidden'>
-        <Swiper
-          style={{
-            '--swiper-navigation-color': '#ff5756',
-            '--swiper-pagination-color': '#ff5756',
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          spaceBetween={30}
-          centeredSlides={true}
-          navigation={true}
-          modules={[Pagination, Navigation, Autoplay]}
-          className='mySwiper'>
-          <SwiperSlide>
-            <div>
-              <img className='opt' src={carruselmobile1} />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <img className='opt' src={carruselmobile2} />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <img className='opt' src={carruselmobile3} />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Slider;
